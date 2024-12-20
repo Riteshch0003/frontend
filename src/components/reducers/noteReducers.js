1a458b23c908339f95d1086689aa97304ca51026
ritesh chopra <riteshch0003@gmail.com>
2024-12-20 12:53:21 +0530

    changes

--- src/components/reducers/noteReducers.js
+++ src/components/reducers/noteReducers.js
@@ -16,16 +16,15 @@ import {
 export const noteListReducer = (state = { notes: [] }, action) => {
   switch (action.type) {
     case NOTES_LIST_REQUEST:
-      return { loading: true, notes: [] }; // Clear previous notes when loading new ones
+      return { loading: true, notes: [] };
     case NOTES_LIST_SUCCESS:
-      return { loading: false, notes: action.payload }; // Set the notes from the API response
+      return { loading: false, notes: action.payload };
     case NOTES_LIST_FAIL:
-      return { loading: false, error: action.payload }; // Set the error if the API call fails
+      return { loading: false, error: action.payload };
     default:
       return state;
   }
 };
-
 export const noteCreateReducer = (state = {}, action) => {
   switch (action.type) {
     case NOTES_CREATE_REQUEST:
